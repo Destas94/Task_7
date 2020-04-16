eaad1ae767e1638fa0da18a8e4819ca3958601be
Dmitry <dim4n94@yandex.ru>
2020-04-16 18:28:43 +0300

    Update code

--- src/Task.java
+++ src/Task.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 
 public class Task {
     public static void main(String[] args) {
+        int X = 5;
+        int Y = 6;
+        int Z = 7;
         Scanner in = new Scanner(System.in);
         System.out.print("Введите number: ");
         int number = in.nextInt();
-        switch(number)
-        {
-            case 5 :
-                break;
-            case 6 :
-                break;
-            case 7 :
-                break;
-            default :
-                System.out.println("Такой константы нет!");
+        if (number==X|number==Y|number==Z){
+            System.out.println("Данное значение имеется в константах");
+            }else{
+            System.out.println("Такой константы нет!");
         }
-        System.out.println("Данное значение имеется в константах");
     }
+
 }
